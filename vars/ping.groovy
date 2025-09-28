88202138bec7f4af2f56116896daf63e3626299f
Steven Min <steven.jsmin@gmail.com>
2025-09-28 20:48:04 +1000

    init

---
+++ vars/ping.groovy
@@ -0,0 +1,26 @@
+def call(Map config) {
+
+    echo "-------------------------------------------------------------------------------------------------- ping.groovy"
+
+
+    // def ARTIFACT_SUB_DIR   = config.ARTIFACT_SUB_DIR
+    // def FILE_NAME          = config.FILE_NAME
+    
+    
+    ansiColor('gnome-terminal') {
+          timestamps {
+
+              sh """
+                  export ANSIBLE_FORCE_COLOR=true
+                  export PYTHONUNBUFFERED=1
+                  echo "Hi from shell command~~~"
+              """
+              
+          }
+    }
+
+    return "Pong~~~";
+    
+}        
+        
+
